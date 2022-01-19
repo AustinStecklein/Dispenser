ec5d721087ba0444131d9fbcff3c0b735d8fd16a
Dale Andreoli <daleandreoli@yahoo.com>
2022-01-19 16:43:00 -0700

    Update test.py

--- dale_GUI_tests/test.py
+++ dale_GUI_tests/test.py
@@ -7,12 +7,13 @@ def click():
 
 window = Tk()
 window.title("Dale's GUI test")
+window.geometry("800x480")
 window.configure(background="black")
 
 image1 = PhotoImage(file="dale_GUI_tests\creedmoor_logo.png")
 Label (window, image=image1, bg="black") .grid(row=0, column=0, sticky=W)
 
-Label(window, text="This is a test message for the display", bg="black", fg="red", font="none 14")  .grid(row=0, column=1, sticky=E)
+Label(window, text="This is a test message", bg="black", fg="red", font="none 14")  .grid(row=0, column=1, sticky=E)
 
 text_entry = Entry(window, width=30, bg="white", fg="black")
 text_entry.grid(row=1, column=0)
