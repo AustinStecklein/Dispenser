cbbd8b6f50e723adcfcdc7d74732fdcf154d951f
--replace-all <astecklein64@gmail.com>
2022-01-19 08:28:40 -0700

    added framework for driver classes

--- dispenser/StepperDrivers.py
+++ dispenser/StepperDrivers.py
@@ -1,5 +1,14 @@
 #import Adafruit_BBIO.PWM as PWM #looks like it runs python 3.7
+import Adafruit_BBIO.PWM as PWM #import PWM library
 
-def startStepper():
-    while(1):
-        print("testing")
+class StepperDriver:
+    def __init__(self):
+        pass #Possiblly turn on pins or driver
+
+    def run(self, speed):
+        #PWM.start(channel, duty, freq=2000, polarity=0)
+        #Channel needs to defined in terms of pin & polarity is direction
+        pass #Running the motor
+
+    def stop (self):
+        pass #define full stop
