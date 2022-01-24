49ee0f52c387fd21375117c8853693bb32fa5148
Dale Andreoli <daleandreoli@yahoo.com>
2022-01-24 15:04:00 -0700

    num pad testing

---
+++ dale_GUI_tests/num_pad.py
@@ -0,0 +1,39 @@
+from tkinter import *
+import tkinter.font as font
+
+# Used this guide:
+# https://www.pythontutorial.net/tkinter/tkinter-object-oriented-frame/
+
+def button_press(btn):
+    print(btn)
+
+class NumPad(Frame):
+    def __init__(self, window):
+        super().__init__(window)
+        self.grid()
+        self.create_buttons()
+
+    def create_buttons(self):
+        button_list = [ '-1',   '+0.1', '+1',
+                        '7',    '8',    '9',
+                        '4',    '5',    '6',
+                        '1',    '2',    '3',
+                        'Back', '0',    'Enter']
+
+        r = 0
+        c = 0
+        button_font = font.Font(family='Arial Unicode MS', size=18, weight='bold')
+        for button in button_list:
+            self.btn =  Button(self, 
+                                text=button,
+                                bg='red',
+                                fg='white',
+                                width=6, 
+                                height=2, 
+                                command=lambda txt=button:button_press(txt)) 
+            self.btn['font'] = button_font
+            self.btn.grid(row=r, column=c)
+            
+            c = (c + 1) % 3
+            if c == 0:
+                r += 1
