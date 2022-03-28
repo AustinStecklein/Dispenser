dd6175fcee79e475cd2dbb79262c402ca92f05d2
AustinStecklein <52940880+AustinStecklein@users.noreply.github.com>
2022-03-28 16:41:21 -0600

    Delete dispenser directory

--- dispenser/App.py
+++
@@ -1,8 +0,0 @@
-from GUI import GUI
-from Eventhandler import Eventhandler
-
-
-if __name__ == '__main__':
-    event_handler = Eventhandler()
-    gui = GUI(event_handler)
-    print("Application started")
