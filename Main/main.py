2a2819b73f2102db377e4d955d729bea33d55d57
jonArbiter <phillipsjonathanjoseph@gmail.com>
2022-03-28 16:19:23 -0600

    Full screen

--- Main/main.py
+++ Main/main.py
@@ -63,6 +63,7 @@ if __name__ == "__main__":
     # Starts the Program and labels the Screen as Dispenser
     root = tk.Tk()
     root.title('Dispenser')
+    root.attributes('-fullscreen',True)
 
     # Sets the MainView Class on the root, ensuring it stays on screen on every page while filling the screen
     main = MainView(root)
